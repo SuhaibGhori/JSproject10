1ce7556264ddd2b7dba1702de3de5260f49f1a96
SuhaibGhori <ghorisuhaib09@gmail.com>
2024-10-24 16:17:06 +0500

    UPDATED

---
+++ app.js
@@ -0,0 +1,27 @@
+var input = document.getElementById("inputField");
+
+function getValue(number) {
+ input.value += number;   
+}
+
+function clrAll() {
+    input.value = "";
+}
+
+
+function deleteChar() {
+    input.value = input.value.slice(0,-1)
+}
+
+function square() {
+    input.value = input.value * input.value;
+}
+
+function equal() {
+    if (input.value) {
+        input.value =eval(input.value);
+    }
+    else{
+        input.value = "Syntax Error"
+    }
+}
